03f3337591bbec092d64a92e2744a53ec3529e5c
georgiaclemencon <georgia.clemencon@isen.yncrea.fr>
2023-12-09 11:37:41 +0100

    Affichage des Séries

--- src/app/shows/shows.component.ts
+++ src/app/shows/shows.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-shows',
   templateUrl: './shows.component.html',
   styleUrls: ['./shows.component.scss']
 })
-export class ShowsComponent {
+export class ShowsComponent implements OnInit{
+  TvShowList : any = ''
+  sub : any = null
 
+  constructor(private dataService : DataService){
+
+  }
+  ngOnInit() {
+    this.getTvShowPopular()
+  }
+
+  getTvShowPopular(){
+    this.sub = this.dataService.getTvPopular().subscribe((data) => 
+    this.TvShowList = data)
+    
+  }
 }
